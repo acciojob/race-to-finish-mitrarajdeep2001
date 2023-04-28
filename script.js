edf9cb98430048ee40df755664ca58db4e637615
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-28 21:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ window.promises = [];
 const p1 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve(1)
-	}, Math.random() * 5)
+	}, getRandomIntInclusive(1, 5))
 })
 const p2 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve(2)
-	}, Math.random() * 5)
+	}, getRandomIntInclusive(1, 5))
 })
 const p3 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve(3)
-	}, Math.random() * 5)
+	}, getRandomIntInclusive(1, 5))
 })
 const p4 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve(4)
-	}, Math.random() * 5)
+	}, getRandomIntInclusive(1, 5))
 })
 const p5 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve(5)
-	}, Math.random() * 5)
+	}, getRandomIntInclusive(1, 5))
 })
-      
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+                      
 promises.push(p1, p2, p3, p4, p5)
 Promise.any(promises).then((result) => {
 	document.getElementById('output').innerText = result;
